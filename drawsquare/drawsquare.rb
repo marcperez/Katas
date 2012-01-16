afb5307b5fcbbce2cbf59841a8b3140e49d31651
Marc Pérez <marcperez2000@gmail.com>
2012-01-16 10:34:06 +0100

    Refactor

--- drawsquare/drawsquare.rb
+++ drawsquare/drawsquare.rb
@@ -1,10 +1,13 @@
 # Write a program that reads a number and draw a square of n*n characters
 
-n = ARGV[0].to_i
-
-n.times do |i|
-	n.times do
-		print "#    "
+def draw_square n
+	n.times do |i|
+		n.times do
+			print "#    "
+		end
+		puts ""
 	end
-	puts ""
 end
+
+n = ARGV[0].to_i
+draw_square n
