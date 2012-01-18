bac916336a4d36adc18ff3005600cd342545f662
Marc Pérez <marcperez2000@gmail.com>
2012-01-19 00:34:07 +0100

    FizzBuzz Rspec & refactor
    Added fizzbuzz_spec.rb & refactor

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -2,20 +2,27 @@
 #  But for multiples of three print "Fizz" instead of the
 #  number and for the multiples of five print "Buzz".
 #  For numbers which are multiples of both three and five 
-#  print "FizzBuzz?".
+#  print "FizzBuzz".
 #  Stage 2
 #    A number is fizz if it is divisible by 3 or if it has a 3 in it
 #    A number is buzz if it is divisible by 5 or if it has a 5 in it
 
+class FizzBuzz
+	def calculate(i)
+		if ((i % 5)  ==0) and ((i % 3) == 0) or (i.to_s.include? "3" and i.to_s.include? "5")
+		 	"FIZZBUZZ"
+		elsif (i%3 == 0) or i.to_s.include? "3"
+			"FIZZ"
+		elsif i%5 == 0 or i.to_s.include? "5"
+			"BUZZ"
+		else
+			i
+		end
+	end
+end
+
+fizzbuzz = FizzBuzz.new
 
 (1..100).each do |i|
-	if ((i % 5)  ==0) and ((i % 3) == 0) or (i.to_s.include? "3" and i.to_s.include? "5")
-	 	puts "FIZZBUZZ"
-	elsif (i%3 == 0) or i.to_s.include? "3"
-		puts "FIZZ"
-	elsif i%5 == 0 or i.to_s.include? "5"
-		puts "BUZZ"
-	else
-		puts i
-	end
+	puts fizzbuzz.calculate(i)
 end
