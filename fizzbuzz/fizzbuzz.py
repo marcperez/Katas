0ff9f7986eda98f0129488f6ad4e5a809b1ee765
Marc Pérez <marcperez2000@gmail.com>
2012-03-10 20:03:41 +0100

    tabs to spaces

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -8,11 +8,11 @@
 #    A number is buzz if it is divisible by 5 or if it has a 5 in it
 
 for x in range(1,101):
-	if x % 15 == 0:
-		print "FIZZBUZZ"
-	elif x % 3 == 0:
-		print "FIZZ"
-	elif x % 5 == 0:
-		print "BUZZ"
-	else:
-		print x
+  if x % 15 == 0:
+    print "FIZZBUZZ"
+  elif x % 3 == 0:
+    print "FIZZ"
+  elif x % 5 == 0:
+    print "BUZZ"
+  else:
+    print x
