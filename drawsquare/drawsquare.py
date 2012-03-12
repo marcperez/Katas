114e306ffb08b441a64ea5896eeb60321945eb80
Marc Pérez <marcperez2000@gmail.com>
2012-03-12 01:27:04 +0100

    tabs to spaces

--- drawsquare/drawsquare.py
+++ drawsquare/drawsquare.py
@@ -1,9 +1,9 @@
 # Write a program that reads a number and draw a square of n*n characters
 
 def draw_square(n):
-	for x in range(1,n+1):
-		for x in range(1,n+1):
-			 print "#  ",
-		print ""
+  for x in range(1,n+1):
+    for x in range(1,n+1):
+       print "#  ",
+    print ""
 
 draw_square(10)
